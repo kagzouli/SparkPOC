ec330a1e0a5b6bd968ed79401ffb184336ca9adc
unknown <koko_266@@hotmail.fr>
2019-01-19 16:50:32 +0100

    Modifications corrections

--- src/main/java/com/exakaconsulting/spark/poc/BatchTestKarimRegistrator.java
+++ src/main/java/com/exakaconsulting/spark/poc/BatchTestKarimRegistrator.java
@@ -39,6 +39,7 @@ public class BatchTestKarimRegistrator implements KryoRegistrator {
 		kryo.register(org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema.class);
 		kryo.register(org.apache.spark.sql.types.LongType$.class);
 		kryo.register(org.apache.spark.sql.types.ArrayType.class);
+		kryo.register(org.apache.spark.sql.types.NullType$.class);
 		
 		
 		try {
